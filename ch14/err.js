0bc5c00d9972a84d30cd825568097796fc251a1a
jinyoungSul <jysul8230@gmail.com>
2017-08-30 23:54:54 +0900

    node study

--- ch14/err.js
+++ ch14/err.js
@@ -7,18 +7,18 @@ app.listen(3000);
 
 function work(req,res,next){
     var value = parseInt(req.params.value);
-
+    
     if(!value){
-        var err = new Exception('숫자가 아닙니다.');
+        var err = new Error('숫자가 아닙니다.');
         next(err);
         return;
-    } 
+    }
     res.send('result : '+value);
 }
 function errorHandler(err,req,res,next){
    if(err){
        console.error(err.stack);
-       res.status(500).send({ error: 'Something failed!' });
+       res.status(500).send({ error: err.message });
        return;
    }
 }
